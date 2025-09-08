f6c0c3d9bb6ebe90599139006adac2d5b210a64b
gustavhammarin <gustav.hammarin@gmail.com>
2025-09-08 12:57:45 +0200

    changed the state logic

--- script.js
+++ script.js
@@ -1,92 +1,105 @@
 //HTML ELEMENTS
-const root = document.getElementById("root")
-
-const loginForm = document.createElement("form")
-loginForm.classList.add("login-form")
-
-const usernameInput = document.createElement("input")
-usernameInput.name = "username"
-usernameInput.placeholder = "username"
-usernameInput.type = "text"
-
-const passwordInput = document.createElement("input")
-passwordInput.name = "password"
-passwordInput.placeholder = "password"
-passwordInput.type = "password"
-
-const loginButton = document.createElement("button")
-loginButton.textContent = "Login"
-loginButton.type = "submit"
-
-loginForm.appendChild(usernameInput)
-loginForm.appendChild(passwordInput)
-loginForm.appendChild(loginButton)
+const root = document.getElementById("root");
 
 //GLOBAL VARIABLES
-let isLoginModal = true
-console.log(isLoginModal)
 
 //EVENTLISTENERS
-loginForm.addEventListener("submit", (e) => {
-    e.preventDefault()
-
-    const data = new FormData(loginForm)
-
-    let username = data.get("username")
-    let password = data.get("password")
-
-    let validation = validateCredentials(username, password)
-
-    if (!validation){
-        console.log("validation problem")
-    } else {
-        saveUsernameToLocalStorage(username)
-        isUserLoggedIn()
-    }
-
-    console.log(username)
-    console.log(password)
-})
 
 //FUNCTIONS
 const isUserLoggedIn = () => {
-    let username = localStorage.getItem("username");
-    console.log(username)
-    if (username) {
-        isLoginModal = false
-    } else {
-        isLoginModal = true
-    }
-}
-
-const Login = () => {}
-
-const LogOut = () => {}
-
-const isLoginModalOpen = () => {
-  if (isLoginModal) {
-    root.appendChild(loginForm)
+  let username = localStorage.getItem("username");
+  console.log(username);
+  if (username) {
+    return true;
   }
-  console.log(isLoginModal)
+  return false;
+};
+
+const LogOut = () => {
+  localStorage.removeItem("username");
+};
+const OpenLoginForm = () => {
+  if (isUserLoggedIn()) return;
+
+  const loginForm = document.createElement("form");
+  loginForm.classList.add("login-form");
+
+  const usernameInput = document.createElement("input");
+  usernameInput.name = "username";
+  usernameInput.placeholder = "username";
+  usernameInput.type = "text";
+
+  const passwordInput = document.createElement("input");
+  passwordInput.name = "password";
+  passwordInput.placeholder = "password";
+  passwordInput.type = "password";
+
+  const loginButton = document.createElement("button");
+  loginButton.textContent = "Login";
+  loginButton.type = "submit";
+
+  loginForm.appendChild(usernameInput);
+  loginForm.appendChild(passwordInput);
+  loginForm.appendChild(loginButton);
+
+  root.appendChild(loginForm);
+
+  loginForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const data = new FormData(loginForm);
+
+    let username = data.get("username");
+    let password = data.get("password");
+
+    let validation = validateCredentials(username, password);
+
+    if (!validation) {
+      console.log("validation problem");
+    } else {
+      saveUsernameToLocalStorage(username);
+      closeLoginForm()
+    }
+
+    console.log(username);
+    console.log(password);
+  });
+};
+const closeLoginForm = () => {
+  const form = document.querySelector(".login-form");
+  if (form) root.removeChild(form);
+};
+const OpenWelcomePage = () => {
+    if (!isUserLoggedIn()) return;
+
+    const homeContainer = document.createElement("div");
+    homeContainer.classList.add("home-container");
+
+    const welcomeText = document.createElement("h1")
+    welcomeText.textContent = "Welcome"
+
+    const logoutButton = document.createElement("button")
+    logoutButton.textContent = "Logout"
+
+    homeContainer.appendChild(welcomeText)
+    homeContainer.appendChild(logoutButton)
 }
 const validateCredentials = (username, password) => {
-    if (username.length <= 6 || username === ""){
-        console.log("error username is less than 6 chars long")
-        return false;
-    }
-    if (password.length <= 6 || password === ""){
-        console.log("error username is less than 6 chars long")
-        return false;
-    }
-    return true;
-}
+  if (username.length <= 6 || username === "") {
+    console.log("error username is less than 6 chars long");
+    return false;
+  }
+  if (password.length <= 6 || password === "") {
+    console.log("error username is less than 6 chars long");
+    return false;
+  }
+  return true;
+};
 const saveUsernameToLocalStorage = (username) => {
-    localStorage.setItem("username", username);
-}
-isUserLoggedIn()
-isLoginModalOpen()
+  localStorage.setItem("username", username);
+};
 
-
-
-
-
+const renderPage = () => {
+  OpenLoginForm();
+};
+renderPage();
