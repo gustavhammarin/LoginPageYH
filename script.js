88c550c4c51c1f7f921636e7f0115823cb0bfc06
gustavhammarin <gustav.hammarin@gmail.com>
2025-09-22 20:27:01 +0200

    added register

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const root = document.getElementById("root");
 
 //GLOBAL VARIABLES
 const state = {
-  loggedIn: !!localStorage.getItem("username")
+  loggedIn: !!localStorage.getItem("username"),
 };
 
 const credentials = {
@@ -11,22 +11,35 @@ const credentials = {
   password: "1234",
 };
 
+let isLoginFormOpen = true;
+let isRegistrationFormOpen = false;
+
+const users = []
+
+class User {
+  constructor(username, password){
+    this.username = username,
+    this.password = password
+  }
+}
+
 render();
 
 //FUNCTIONS
-function OpenLoginForm () {
+function OpenLoginForm() {
+  isLoginFormOpen = true;
   const loginForm = document.createElement("form");
   loginForm.classList.add("login-form");
 
-  const heading = document.createElement("h1")
-  heading.textContent = "Login"
+  const heading = document.createElement("h1");
+  heading.textContent = "Login";
 
   const usernameInput = document.createElement("input");
   usernameInput.name = "username";
   usernameInput.placeholder = "username";
   usernameInput.type = "text";
-  const errorLabel = document.createElement("label")
-  errorLabel.id = "error-label"
+  const errorLabel = document.createElement("label");
+  errorLabel.id = "error-label";
 
   const passwordInput = document.createElement("input");
   passwordInput.name = "password";
@@ -37,15 +50,22 @@ function OpenLoginForm () {
   loginButton.textContent = "Login";
   loginButton.type = "submit";
 
-  loginForm.appendChild(heading)
-  loginForm.appendChild(errorLabel)
-  loginForm.appendChild(usernameInput);
-  loginForm.appendChild(passwordInput);
-  loginForm.appendChild(loginButton);
+  const registrationFormLink = document.createElement("button");
+  registrationFormLink.textContent = "Register";
+
+  loginForm.append(
+    heading,
+    errorLabel,
+    usernameInput,
+    passwordInput,
+    loginButton,
+    registrationFormLink
+  );
 
   root.appendChild(loginForm);
 
   //EVENTLISTENER
+  registrationFormLink.addEventListener("click", (e) => toggleForms(e));
   loginForm.addEventListener("submit", (e) => {
     e.preventDefault();
 
@@ -61,15 +81,71 @@ function OpenLoginForm () {
       state.loggedIn = true;
       render();
     } else {
-        showErrorToast(error)
+      showErrorToast(error);
     }
   });
-};
-function closeLoginForm () {
+}
+function OpenRegistrationForm() {
+  isRegistrationFormOpen = true;
+
+  const registrationForm = document.createElement("form");
+  registrationForm.classList.add("login-form");
+
+  const heading = document.createElement("h1");
+  heading.textContent = "Register";
+
+  const usernameInput = document.createElement("input");
+  usernameInput.name = "username";
+  usernameInput.placeholder = "username";
+  usernameInput.type = "text";
+  const errorLabel = document.createElement("label");
+  errorLabel.id = "error-label";
+
+  const passwordInput = document.createElement("input");
+  passwordInput.name = "password";
+  passwordInput.placeholder = "password";
+  passwordInput.type = "password";
+
+  const loginButton = document.createElement("button");
+  loginButton.textContent = "Register";
+  loginButton.type = "submit";
+
+  const loginFormLink = document.createElement("button");
+  loginFormLink.textContent = "Login";
+
+  registrationForm.append(
+    heading,
+    errorLabel,
+    usernameInput,
+    passwordInput,
+    loginButton,
+    loginFormLink
+  );
+
+  root.appendChild(registrationForm);
+
+  //EventListeners
+  loginFormLink.addEventListener("click", (e) => toggleForms(e));
+  registrationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    
+    const data = new FormData(registrationForm);
+
+    const user = new User(data.get("username"), data.get("password"));
+    users.push(user)
+    localStorage.removeItem("users")
+    localStorage.setItem("users", JSON.stringify(users))
+    toggleForms(e);
+
+  })
+}
+function close() {
   const form = document.querySelector(".login-form");
   if (form) root.removeChild(form);
-};
-function OpenWelcomePage () {
+  const home = document.querySelector(".home-container");
+  if (home) root.removeChild(home);
+}
+function OpenWelcomePage() {
   const homeContainer = document.createElement("div");
   homeContainer.classList.add("home-container");
 
@@ -79,8 +155,7 @@ function OpenWelcomePage () {
   const logoutButton = document.createElement("button");
   logoutButton.textContent = "Logout";
 
-  homeContainer.appendChild(welcomeText);
-  homeContainer.appendChild(logoutButton);
+  homeContainer.append(welcomeText, logoutButton);
 
   root.appendChild(homeContainer);
 
@@ -90,35 +165,51 @@ function OpenWelcomePage () {
     state.loggedIn = false;
     render();
   });
-};
-function closeWelcomePage () {
-  const home = document.querySelector(".home-container");
-  if (home) root.removeChild(home);
-};
-function validateCredentials (username, password) {
-    const error = "Incorrect username or password"
-    if (username != credentials.username || password != credentials.password){
-        return error
-    }
-};
-function showErrorToast (error) {
-    const errorLabel = document.getElementById("error-label")
-    errorLabel.textContent = error
 }
-function saveUsernameToLocalStorage (username) {
-    localStorage.setItem("username", username);
-};
-function clearLocalstorage () {
-    localStorage.removeItem("username");
-};
-function render () {
-  closeLoginForm();
-  closeWelcomePage();
+
+function validateCredentials(username, password) {
+  const error = "Incorrect username or password";
+  const users = JSON.parse(localStorage.getItem("users"));
+  const isUser = !!users.find(user => user.username === username && user.password === password);
+  if(!isUser){
+    return error
+  }
+  
+/*   if (username !== credentials.username || password !== credentials.password) {
+    return error;
+  } */
+}
+function showErrorToast(error) {
+  const errorLabel = document.getElementById("error-label");
+  errorLabel.textContent = error;
+}
+function saveUsernameToLocalStorage(username) {
+  localStorage.setItem("username", username);
+}
+function clearLocalstorage() {
+  localStorage.removeItem("username");
+}
+function toggleForms(e) {
+  e.preventDefault();
+  if (isLoginFormOpen) {
+    isLoginFormOpen = false;
+    isRegistrationFormOpen = true;
+  } else if (isRegistrationFormOpen) {
+    isLoginFormOpen = true;
+    isRegistrationFormOpen = false;
+  }
+  render()
+}
+function render() {
+  close();
 
   if (!state.loggedIn) {
-    OpenLoginForm();
+    if (isLoginFormOpen && !isRegistrationFormOpen) {
+      OpenLoginForm();
+    } else if (!isLoginFormOpen && isRegistrationFormOpen) {
+      OpenRegistrationForm();
+    }
   } else {
     OpenWelcomePage();
   }
-};
-
+}
